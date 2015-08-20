6f940f749e5c1d20fefae2024eebbbfb1da887a5
= <=>
2015-08-20 12:08:38 -0400

    Finish user & session logic

--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login!(@user)
       redirect_to user_url(@user.id)
     else
       flash.now[:errors] = @user.errors.full_messages
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     render :show
   end
 
