4a1a658dadd85d309cf548fea40f2fa176658113
= <=>
2015-08-20 15:47:21 -0400

    Create Notes model and add associations to track and user

--- MusicApp/app/models/album.rb
+++ MusicApp/app/models/album.rb
@@ -9,4 +9,5 @@ class Album < ActiveRecord::Base
 
   has_many :tracks,
     dependent: :destroy
+
 end
--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
 
   attr_reader :password
 
+  has_many :notes
+
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
   end
